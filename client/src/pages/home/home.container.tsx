76d941f9ffea6aaa6e34ff48223962ba95cba1ee
Michael Hayden <mhayden@michaelhayden.net>
2023-03-04 20:09:33 +0000

    init home reducers

--- client/src/pages/home/home.container.tsx
+++ client/src/pages/home/home.container.tsx
@@ -3,8 +3,16 @@ import { RootState } from "../../reducers/root.reducer";
 import { AppDispatch } from "../../reducers/store";
 import { HomeActionProps, HomeFeature, HomeProps } from "./home.feature";
 
-const mapStateToProps = (state: RootState): HomeProps => ({});
+const mapStateToProps = (state: RootState): HomeProps => ({
+    // selectedStartDate: state.selected.startDate,
+    // selectedFinishDate: state.selected.FinishDate
+});
 
-const mapDispatchToProps = (dispatch: AppDispatch): HomeActionProps => ({});
+const mapDispatchToProps = (dispatch: AppDispatch): HomeActionProps => ({
+    updateStartDate: () => console.log('updateStartDate'),
+    getStartDate: () => console.log('getStartDate'),
+    updateFinishDate: () => console.log('updateFinishDate'),
+    getFinishDate: () => console.log('getFinishDate'),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeFeature);
