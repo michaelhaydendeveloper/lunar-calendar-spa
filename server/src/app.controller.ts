e3afbfcad4712c300c5422d9dc88b629e4cead48
Michael Hayden <mhayden@michaelhayden.net>
2023-03-04 19:30:59 +0000

    play with Interface design pattern

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface IResponse {
+  message: string,
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): IResponse {
+    return { message: this.appService.getHello() };
   }
 }
