dc44181cd0ca9b24acc33b3cbca057b62a083239
Michael Hayden <mhayden@michaelhayden.net>
2023-03-12 20:16:11 +0000

    move forgot password link

--- client/src/pages/login/login.feature.tsx
+++ client/src/pages/login/login.feature.tsx
@@ -37,7 +37,7 @@ export const LoginFeature: FC<LoginProps & LoginActionProps> = () => {
                         </div>
                         <button>Log in</button>
                         <div className="register">
-                            <p>Don't have a account? <a href="#">Register</a></p>
+                            <p>Don't have an account? <a href="#">Register</a></p>
                             <p><a href="#">Forgot Password</a></p>
                         </div>
                     </form>
